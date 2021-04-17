6997ca7d6e37c844f4f45c317705b4f28e370fcd
DeyBo <b.m199802@gmail.com>
2021-04-17 21:39:34 +0200

    Update game component by adding character and keyboard event service

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,16 +1,49 @@
 import React, {Component} from "react";
 import './Game.css';
+import Character from "../Character/Character";
+import KeyboardEventService from "../../services/KeyboardEventService";
 
 class Game extends Component {
 
+    protected speed = 32;
+    state = {
+        characterPosition: {x: 0, y: 0}
+    };
+
+    constructor(private keyboardEventService: KeyboardEventService) {
+        super(keyboardEventService);
+        this.keyboardEventService = new KeyboardEventService();
+    }
+
     componentDidMount() {
+        this.initCharacterMovement();
+    }
+
+    initCharacterMovement() {
+        document.addEventListener('keydown', event => {
+            let {x, y} = this.state.characterPosition;
+            const {xDir, yDir} = this.keyboardEventService.getMovingDirection(event.key);
+            this.keyboardEventService.saveHeldKey(event.key);
+            this.setState({
+                characterPosition: {
+                    x: x + this.speed * xDir,
+                    y: y + this.speed * yDir
+                }
+            });
+        });
+        document.addEventListener('keyup', event => {
+            this.keyboardEventService.removeHeldKey(event.key);
+        });
     }
 
     render() {
         return (
-            <svg id="game" width={32*10} height={32*10}>
-                <rect x={0} y={0} width={32} height={32}/>
-            </svg>
+            <div id={'game'}>
+                <Character
+                    x={this.state.characterPosition.x}
+                    y={this.state.characterPosition.y}
+                />
+            </div>
         );
     }
 }
