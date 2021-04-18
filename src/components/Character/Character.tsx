ef9bf04dcbd9098979d05429ad7a43cc07632ff7
DeyBo <b.m199802@gmail.com>
2021-04-18 19:09:51 +0200

    Update Character function to simplify position attribution

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,15 +1,13 @@
 import './Character.css';
 
 function Character(props: {x: number, y: number}) {
-    const characterElement = document.getElementById('character');
-    if (characterElement) {
-        characterElement.style.top = props.y + 'px';
-        characterElement.style.left = props.x + 'px';
-    }
-
-    return (
-        <svg id={'character'} width={32} height={32}>
-            <rect width={32} height={32}/>
+    return(
+        <svg width={32} height={32} x={props.x} y={props.y}>
+            <rect width={8} height={8} x={0} y={0}/>
+            <rect width={8} height={8} x={24} y={0}/>
+            <rect width={8} height={8} x={0} y={24}/>
+            <rect width={8} height={8} x={24} y={24}/>
+            <rect width={8} height={8} x={12} y={12}/>
         </svg>
     )
 }
